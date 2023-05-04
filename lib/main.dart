47cfbb37991a5d2b53497d77c7079d70b74bcf0e
Olamakhlouf <olamoatasem@gmail.com>
2023-05-04 09:45:23 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/intro_screen.dart';
+import 'screens/bmi_screen.dart';
+void  main() {
+  runApp(const GlobeApp());
+}
+
+
+class GlobeApp extends StatelessWidget {
+  const GlobeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.blueGrey),
+        routes: {
+          '/': (context) => const IntroScreen(),
+          '/bmi': (context) => const BmiScreen()
+        }, 
+        initialRoute: '/',
+        );
+    //home: IntroScreen());
+  }
+}
